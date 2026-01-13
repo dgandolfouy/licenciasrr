df0baba918c039f429aa170b2c96b74bd0de3bf1
dgandolfouy <daniel.gandolfo@gmail.com>
2026-01-12 22:56:37 -0300

    Actualizar index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './App.css'; // <--- ESTA es la línea mágica que carga tus diseños
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
